f1dee5c27d855e97bcb52302a3610f187f3626ab
haemiii <73984822+haemiii@users.noreply.github.com>
2023-04-16 14:14:45 +0900

    init

---
+++ clone_coding/src/test/java/instagram/clone_coding/CloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.clone_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
